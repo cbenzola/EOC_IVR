153682c5aaebd8405020004a0114ea8f5d05a443
Chris Benzola <56400496+cbenzola@users.noreply.github.com>
2021-07-21 09:34:46 -0400

    Update purge_audio.sh

--- scripts/purge_audio.sh
+++ scripts/purge_audio.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find /var/www/html/eoc/audio -type f -name '*_[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]_[0-9][0-9]_[0-9][0-9].wav' -exec sh -c 'fdate="${1%_[0-9][0-9]_[0-9][0-9].wav}"; fdate="${fdate##*_}"; [ "$fdate" "<" "$(date  -d "0 days ago" "+%m-%d-%Y")" ] && rm "$1"' find-sh {} \;
+find /var/www/html/eoc/audio -type f -name '*_[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]_[0-9][0-9]_[0-9][0-9].wav' -exec sh -c 'fdate="${1%_[0-9][0-9]_[0-9][0-9].wav}"; fdate="${fdate##*_}";ndate=$(echo $fdate | sed 's/-/\//g'); n2date=$(date -d $ndate +%Y/%m/%d);  [ "$n2date" "<" "$(date  -d "0 days ago" "+%Y/%m/%d")" ] && rm "$1"' find-sh {} \;
