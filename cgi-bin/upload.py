dbeecb674675e8433cbfd751436547d5e3b81c35
Chris Benzola <56400496+cbenzola@users.noreply.github.com>
2021-07-21 09:25:57 -0400

    Update and rename upload2.py to upload.py

--- cgi-bin/upload.py
+++ cgi-bin/upload.py
@@ -3,8 +3,6 @@
 import cgi, cgitb, os, sys, requests
 import sys, urllib
 from datetime import date, datetime
-from pytz import timezone
-from dateutil.relativedelta import relativedelta
 
 # This is the CGI script that uploads and renames the audio file to the server #
 
@@ -25,24 +23,41 @@ def save_uploaded_file():
     ssn = form.getfirst('ssn')
     
     # Grab the date from the URL #
-    date = form.getfirst('date')
+    get_date = form.getfirst('date')
     
     # Format the date in mm-dd format
-    d = '-'.join([date[:2], date[2:4], date[4:]])
+    format_date = '-'.join([get_date[:2], get_date[2:4]])
+    
+    # Convert the formatted date into a date object
+    d = datetime.strptime(format_date, '%m-%d')
     
     # Now we need to get the year and time so we can add it into the filename #
     # The time is needed, so that we do not have any duplicate files #
     
-    # The today variable grabs the current date and time #
-    # But since we only care about getting the year and time, we will have today2 format it in yyyy_hour_minute format 
-    tz= timezone('EST')
-    today = datetime.now(tz)
+    # This is where it may get confusing. If a caller schedules a ride for next year, we have to make sure we increment the year
+    # So to do that, I had to do a little trickery 
     
-    today2 =today.strftime("%Y_%H_%M")
+
+    # The now variable gets the todays date and time
+    now = datetime.now()
+    # Time gets the current hour and minutes. We will insert this into the filename to, to ensure that there are no duplicates
+    time = now.strftime('%H_%M')
+    # The today variable grabs the current month #   
+    today = datetime.now().month
+    # get_year grabs the current year from the now variable and converts it into a string. This will be used when we are renaming the file
+    get_year =now.strftime("%Y")
+    # I created another year variable, because when we have to increment the year, it cannot be a string
+    year = date.today().year
     
-    if d < today.strftime("%m-%d-%Y"):
-       today2 += relativedelta(years=1)
+    # increment_year gets todays date, but takes the year variable that we set before and adds 1 to it 
+    increment_year = date.today().replace(year + 1)
     
+    # If the month that the user entered has already passed, increment the year. If it isn't, keep the year the same
+    if d.month < today:
+        get_year= increment_year
+    else:
+        get_year = now
+        
     # form_file grabs the submitted audio file from the system #
     # sound_variable is the name of the input that the phone system stores the audio file in when it sent to us #
     form_file = form['sound_variable']
@@ -54,7 +69,7 @@ def save_uploaded_file():
         open( '../audio/' + fn, 'wb').write(form_file.file.read())
         
         # When the file is inserted, rename it in ssn_mm-dd-yyyy_H_M.wav format #
-        os.rename('../audio/' + fn, '../audio/' + ssn + '_' + d + today2 + '.wav')
+        os.rename('../audio/' + fn, '../audio/' + ssn + '_' + format_date + '-' + get_year.strftime("%Y") + '_' + time + '.wav')
 
         message = 'The file "' + fn + '" was uploaded successfully'
     else:
